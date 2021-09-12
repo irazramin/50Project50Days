8c687bbaa21ed054fdca36cc50817ade9e625985
irazramin <irazramin@gmail.com>
2021-09-12 11:04:34 +0600

    day 18 project 18

---
+++ day18 BackgroundSlider/js/app.js
@@ -0,0 +1,37 @@
+const body = document.querySelector("body");
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;
+
+rightBtn.addEventListener("click", () => {
+  activeSlide++;
+  if (activeSlide > slides.length - 1) {
+    activeSlide = 0;
+  }
+  console.log(activeSlide);
+  setBgSlide();
+  setActiveSlide();
+});
+
+leftBtn.addEventListener("click", () => {
+  activeSlide--;
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+  setBgSlide();
+  setActiveSlide();
+});
+
+setBgSlide();
+
+function setBgSlide() {
+  body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
+}
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+
+  slides[activeSlide].classList.add("active");
+}
