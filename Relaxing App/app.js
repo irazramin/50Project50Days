d93e498157bd3e61a00d1a921ef003a795a3af79
irazramin <irazramin@gmail.com>
2021-10-28 12:11:39 +0600

    lyrics app relaxing app

---
+++ Relaxing App/app.js
@@ -0,0 +1,25 @@
+const container = document.getElementById('container');
+const text = document.getElementById('text');
+
+const totalTime = 7500;
+const breathTime = (totalTime / 5) *2;
+const holdTime = breathTime / 2;
+
+
+breathAnimation()
+
+function breathAnimation(){
+    console.log(123)
+    text.innerText = 'Breathe In'
+    container.className = 'container grow';
+    setTimeout(()=>{
+        text.innerText = 'Hold'
+
+        setTimeout(()=>{
+            container.className = 'container shrink';
+            text.innerText = 'Breathe Out'
+        },holdTime)
+    },breathTime)
+}
+
+setInterval(breathAnimation, totalTime);
