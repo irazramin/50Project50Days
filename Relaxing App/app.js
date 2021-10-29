8a6c8bea81334f3f2ef2e3da1461a5004e1d8abb
irazramin <irazramin@gmail.com>
2021-10-29 15:48:10 +0600

    breakout game

--- Relaxing App/app.js
+++ Relaxing App/app.js
@@ -2,24 +2,23 @@ const container = document.getElementById('container');
 const text = document.getElementById('text');
 
 const totalTime = 7500;
-const breathTime = (totalTime / 5) *2;
+const breathTime = (totalTime / 5) * 2;
 const holdTime = breathTime / 2;
 
+breathAnimation();
 
-breathAnimation()
+function breathAnimation() {
+  console.log(123);
+  text.innerText = 'Breathe In';
+  container.className = 'container grow';
+  setTimeout(() => {
+    text.innerText = 'Hold';
 
-function breathAnimation(){
-    console.log(123)
-    text.innerText = 'Breathe In'
-    container.className = 'container grow';
-    setTimeout(()=>{
-        text.innerText = 'Hold'
-
-        setTimeout(()=>{
-            container.className = 'container shrink';
-            text.innerText = 'Breathe Out'
-        },holdTime)
-    },breathTime)
+    setTimeout(() => {
+      container.className = 'container shrink';
+      text.innerText = 'Breathe Out';
+    }, holdTime);
+  }, breathTime);
 }
 
 setInterval(breathAnimation, totalTime);
